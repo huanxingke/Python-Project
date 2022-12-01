fe3a6b6d99508fb61337aea7bfa3d2e196f904cc
huanxingke <1519657825@qq.com>
2022-12-01 22:45:13 +0800

    Default Changelist

--- Dissertation/Learning/pages/5_🌍_Mapping_Demo.py
+++ Dissertation/Learning/pages/5_🌍_Mapping_Demo.py
@@ -1,7 +1,8 @@
+from urllib.error import URLError
+
 import streamlit as st
 import pandas as pd
 import pydeck as pdk
-from urllib.error import URLError
 
 st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
 
