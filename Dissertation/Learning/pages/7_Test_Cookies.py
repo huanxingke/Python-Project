30b782873959a860f66dea1764c1386cdf287d31
huanxingke <1519657825@qq.com>
2022-12-21 20:41:27 +0800

    Default Changelist

--- Dissertation/Learning/pages/7_Test_Cookies.py
+++ Dissertation/Learning/pages/7_Test_Cookies.py
@@ -2,5 +2,17 @@ from StreamlitCookies import StreamlitCookies
 import streamlit as st
 
 
-cookie1 = StreamlitCookies(name="test", act="add")
-cookie1
+with st.form("cookie_manager"):
+    name_input = st.text_input("Cookie Name:", key="name_input")
+    value_input = st.text_input("Cookie Value:", key="value_input")
+    act_input = st.text_input("Action:", key="act_input")
+    if st.form_submit_button("Start"):
+        cookie_response = StreamlitCookies(name=name_input, value=value_input, act=act_input)
+        st.session_state.CookieManager = cookie_response
+
+if st.session_state.get("CookieManager"):
+    st.write(st.session_state.CookieManager)
+    del st.session_state.CookieManager
+
+cookie_response = StreamlitCookies(name="name", value="Streamlit", act="add")
+cookie_response
